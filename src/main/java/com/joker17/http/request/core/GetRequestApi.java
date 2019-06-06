4c2a5a749a3bf532bfc41db9a95680aa56b1cf8b
yyc <417004794@qq.com>
2019-06-06 18:07:28 +0800

    UPDATE..

---
+++ src/main/java/com/joker17/http/request/core/GetRequestApi.java
@@ -0,0 +1,13 @@
+package com.joker17.http.request.core;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@RequiredArgsConstructor(staticName = "of")
+public class GetRequestApi extends BaseRequestApi {
+
+
+}
---
+++ src/main/java/com/joker17/http/request/core/PostRequestApi.java
@@ -0,0 +1,14 @@
+package com.joker17.http.request.core;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@RequiredArgsConstructor(staticName = "of")
+public class PostRequestApi extends BaseRequestApi {
+
+
+
+}
